2f05bf97e5dfad7e5f3684a2781a400a1990ced7
Tapio-adro <terentuk4321@gmail.com>
2023-09-05 15:10:54 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import { Board } from './components/Board';
+import { Board, BoardColumn } from "./assets/shared/types";
+
+function App() {
+  
+
+  return (
+    <>
+      <Board></Board>
+    </>
+  );
+}
+
+export default App;
---
+++ src/assets/shared/types.ts
@@ -0,0 +1,7 @@
+export type Board = {
+  name: String;
+  columns: BoardColumn[];
+}
+export type BoardColumn = {
+  name: String;
+}
